8ea8cecbbd4ad8549e053cffc0e986887122c66d
austin-99 <78673041+austin-99@users.noreply.github.com>
2021-06-09 23:36:30 -0400

    Updated

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+function sayHi() {
+    alert('Hey, I made this alert to tell you that this is the final update so we do not have to message each other. I hope you like it. I will take this alert down later. (Sorry this will keep showing up whenever you press home or more)');
+}
+setTimeout(sayHi, 1000);
