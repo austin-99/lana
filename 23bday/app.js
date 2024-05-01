908462cc3cf8a25a48a892cb3d4a54347432ae8c
austin-99 <78673041+austin-99@users.noreply.github.com>
2024-05-01 19:02:40 -0400

    updated

---
+++ 23bday/app.js
@@ -0,0 +1,20 @@
+function popup(){
+    alert("Baby, before clicking ok, I want you to know how " + 
+    "much you mean to me. This song will describe a small portion " +
+    "of how much I love you. Read the lyrics carefully baby");
+}
+
+function myFunction(){
+    let input = document.querySelector("#userInput");
+    let message = document.querySelector("#message");
+
+    if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
+        message.innerHTML = "I love how perfect you are for me and there’s nothing I want to change about you";
+    }
+    else if (userInput.value == "no" || userInput.value == "No"){
+        message.innerHTML = "please say yes baby it will make you happier";
+    }
+    else{
+        message.innerHTML = "Baby, try again";
+    }
+}
